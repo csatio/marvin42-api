9070909db1ab51fa77aceac1941d17ed393ad769
csatio <csatio@yahoo.com.br>
2021-08-11 01:24:47 -0300

    aaa

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st 
+from datetime import datetime
+import time
 
 st.markdown("# BITCOIN BOT MARVIN 42")
 
+
+def robot():
+  iter=0
+  while iter<3:
+    
+    st.markdown(datetime.now())
+    time.sleep(60)
+    iter=iter+1
+
+if __name__ == '__main__':
+    robot()
+    
